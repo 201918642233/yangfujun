6347c5884da6a8477a60682c300884b7882f724f
201918642233 <56297495+201918642233@users.noreply.github.com>
2019-10-15 11:18:45 +0800

    Add files via upload

---
+++ xingxing/xingxing.cpp
@@ -0,0 +1,20 @@
+// xingxing.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+int main(int argc, char* argv[])
+{
+int i=0;
+ while (i<10)
+{
+ printf("**********\n");
+ i+=1;
+	
+}
+
+ 
+ 
+ return 0;
+}
+
